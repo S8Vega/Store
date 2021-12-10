7eccdd7ca9a671af2010de3c35565275a5e219dd
bsvega <bsvega@bancolombia.com.co>
2021-12-09 19:26:54 -0500

    fix id of file in file-service

--- file-service/src/main/java/com/practice/fileservice/entity/FileEntity.java
+++ file-service/src/main/java/com/practice/fileservice/entity/FileEntity.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.MongoId;
@@ -14,7 +15,7 @@ import org.springframework.data.mongodb.core.mapping.MongoId;
 @Builder
 @Document(collection = "file")
 public class FileEntity {
-    @MongoId
+    @Id
     private String id;
     @Field
     private String base64;

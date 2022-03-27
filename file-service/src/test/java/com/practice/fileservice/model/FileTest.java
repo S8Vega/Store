5e41b98e6556c50f15c890d73fb27196cbd291e0
bsvega <bsvega@bancolombia.com.co>
2022-03-27 10:43:26 -0500

    add test of file-service model

---
+++ file-service/src/test/java/com/practice/fileservice/model/FileTest.java
@@ -0,0 +1,18 @@
+package com.practice.fileservice.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileTest {
+
+	@Test
+	void constructor() {
+		File file = new File();
+
+		assertNull(file.getFileName());
+		assertNull(file.getFileType());
+		assertNull(file.getBase64());
+	}
+
+}

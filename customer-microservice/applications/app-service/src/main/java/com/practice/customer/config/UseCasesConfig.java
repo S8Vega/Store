f331732dac1766fdbdda92c9daf013a75cd14a57
bsvega <bsvega@bancolombia.com.co>
2022-09-18 16:48:05 -0500

    add customer-microservice

---
+++ customer-microservice/applications/app-service/src/main/java/com/practice/customer/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package com.practice.customer.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "com.practice.customer.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
